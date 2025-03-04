854064af4f6588b4f7e67aefc30e463c5cf5b07b
Vinicius Arjonas <viniciusmonteiroarjonasestudos@gmail.com>
2025-03-04 19:20:25 -0300

    commit inicial

---
+++ product.validation.service/src/test/java/payment/system/product/validation/service/ApplicationTests.java
@@ -0,0 +1,13 @@
+package payment.system.product.validation.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
